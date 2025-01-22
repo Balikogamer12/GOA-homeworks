2de6769c1499df9cd8fa649ab5107d5739e9bb68
Balikogamer12 <lukab.1310@gmail.com>
2025-01-22 21:40:07 +0400

    second commit

---
+++ Day 01/Classwork/Lesson 1.py
@@ -0,0 +1,34 @@
+name = "Luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშნელობა
+
+surname = "Baliashvili"
+
+print(name)
+#პრინტ  ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი 
+
+name = "Luka" #ეს არის str (string) ტიპის ცვლადი 
+age = 14 #ეს არის int ( integer ) მთელი რიცხვი 
+height = 184.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_proggraming = True
+is_ugly = False  #snakecase ( უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგ არის ბრჭყალებში მოწცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_proggraming))
+
+print(name + " " + str(age))
+
