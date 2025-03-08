dc4d99a2e9033387936d3e601e1b7733b87955d7
Balikogamer12 <lukab.1310@gmail.com>
2025-03-08 18:42:23 +0400

    commit

---
+++ Day 08/Classwork/Classwork.py
@@ -0,0 +1,36 @@
+# 1) Name = "Nika"
+#naMe = "Luka"
+#NAME = "Data"
+#naME = "Vano"
+#name = "Demetre"
+#NaMe = "Jan"
+#name = "Giorgi"
+
+#რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+
+#2) შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ
+
+#----------------------------------------------------------------------------
+
+   # ANSWERS:
+
+# 1) ამ კოდის მიხედვით, შეიქმნება 7 ცვლადი რადგან, (Name, naMe, NAME, naME, name, NaMe, name) ეს ყველა ცვლადი ცალკე ითვლება, განსხვავდება დიდი/პატარა ასოებით, 
+# Python-ში ცვლადების სახელები დამოკიდებულია რეგისტრზე (case-sensitive) 
+
+#----------------------------------------------------------------------------
+
+# 2) 
+
+first_name = "Luka"
+last_name = "Baliashvili"
+age = 14
+hobby = "rugby"
+city = "Tbilisi"
+
+print(first_name)
+print(last_name)
+print(age)
+print(hobby)
+print(city)
+
+
---
+++ Day 08/Homework/number.py
@@ -0,0 +1,8 @@
+
+num1, num2, num3, num4, num5 = 1, 2, 3, 4, 5
+
+average = (num1 + num2 + num3 + num4 + num5) / 5
+
+result = average ** 3
+
+print(result)
