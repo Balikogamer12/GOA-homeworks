b71f9cd598193078de7e8c84f4baa8095cee82d3
Balikogamer12 <lukab.1310@gmail.com>
2025-03-22 20:51:04 +0400

    commit

---
+++ Day 12/Classwork/classworknew.py
@@ -0,0 +1,8 @@
+number = input("Enter a number: ")
+print(type(number))  # string
+
+number = int(number)
+print(type(number))  # integer
+
+number = float(number)
+print(type(number))  # float
---
+++ Day 12/Homework/height.py
@@ -0,0 +1,7 @@
+height = input("შეიტანეთ თქვენი სიმაღლე: ")
+
+# გარდაქმნა float-ად
+height_float = float(height)
+
+# დაბეჭდვა მონაცემთა ტიპი
+print("სიმაღლის მონაცემთა ტიპი:", type(height_float))
