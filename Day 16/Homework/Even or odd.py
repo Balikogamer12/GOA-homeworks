44dd8b46c4362b0082de843b59164f191597d843
Balikogamer12 <lukab.1310@gmail.com>
2025-04-05 22:47:51 +0400

    commit

---
+++ Day 16/Homework/Even or odd.py
@@ -0,0 +1,8 @@
+# მომხმარებელი შეყავს რიცხვი
+number = int(input("შეიყვანე რიცხვი: "))
+
+# ლუწობა/კენტობა
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
---
+++ Day 16/Homework/password.py
@@ -0,0 +1,11 @@
+# ცვლადში ვინახავთ სახელს
+name = "ლუკა"
+
+# მომხმარებელი შეყავს პაროლი
+password = input("შეიყვანე პაროლი: ")
+
+# პაროლის შემოწმება
+if password == "Lukicha123!":
+    print("Password is correct")
+else:
+    print("Password is incorrect")
